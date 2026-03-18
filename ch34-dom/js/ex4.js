2b12e542fbba54f1529115b185b402c95fbe48fd
최희정 <hee.j0945@gmail.com>
2026-03-18 10:52:00 +0900

    26.03.18

--- ch34-dom/js/ex4.js
+++ ch34-dom/js/ex4.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded",()=>{
+
     const btnNext = document.querySelector("#btn-next")
     const btnPrev = document.querySelector("#btn-prev")
     const train = document.querySelector(".train")
     let count = 0
-    let slided = train.chlidren.length // 리스트의 개수
+    let slides = train.children.length // 리스트의 개수
     train.style.width = `${100*slides}%`
 
 
     btnNext.addEventListener("click",()=>{
         count++
         if(count>slides-1){count=slides-1}
-        //train 클래스가 왼쪽으로 각 1000픽셀만큼 이동
+        // train클래스가 왼쪽으로 1000픽셀 씩 이동
         train.style.transform = `translateX(${-(100/slides)*count}%)`
     })
 
@@ -19,4 +20,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         if(count<0){count=0}
         train.style.transform = `translateX(${-(100/slides)*count}%)`
     })
+
+
 })
--- ch36-swiper-js-api/js/study1.js
+++ ch36-swiper-js-api/js/study1.js
@@ -1,33 +1,38 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-    const station1 = new Swiper('.swiper', {
-        directive: `vertical`,
-        loop: true,
+    const station1 = new Swiper('.station',{
+        direction:'vertical',
+        loop:true,
         slidesPerView:1.2,
-        spaceBetween: 20,
+        spaceBetween:20,
         speed:1000,
         pagination: {
             el: ".swiper-pagination",
         },
     });
 
-    const station2 = new Swiper('.swiper2', {
-        directive: `horizontal`,
-        loop: true,
+    const station2 = new Swiper('.station2',{
+        direction:'horizontal',
+        loop:true,
         slidesPerView:1.2,
-        spaceBetween: 20,
+        spaceBetween:20,
         speed:1000,
         navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
         pagination: {
             el: ".swiper-pagination",
+            clickable: true,
         },
         autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+            delay: 2500,
+            disableOnInteraction: false,
+        },
     });
-      
+
+
+
+    
+
 })
